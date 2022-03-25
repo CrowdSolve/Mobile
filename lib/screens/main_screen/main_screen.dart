3484ffa74dbbaaf9a636ff575709d62b101fe124
Youssef Lasheen <youssef@lasheen.dev>
2022-03-25 02:17:13 -0700

    Remove default code and add main_screen

---
+++ lib/screens/main_screen/main_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/widgets.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("What do you want to know?")
+    );
+  }
+}

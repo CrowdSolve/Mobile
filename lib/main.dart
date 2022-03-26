d9aa646dc4f4415a313d8653138e9eee8025360e
Youssef Lasheen <youssef@lasheen.dev>
2022-03-26 03:56:46 -0700

    Set the main function to Main Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cs_mobile/screens/main_screen/main_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark(),
-      home: const Text('Flutter Demo Home Page'),
+      home: const MainScreen(),
     );
   }
 }

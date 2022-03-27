04262e0c100fc721792a402c4ac4d1b32f7f4053
Youssef Lasheen <youssef@lasheen.dev>
2022-03-28 00:04:13 +0200

    Add auth managment with riverpod and shared_prefrences

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,49 @@
+import 'package:cs_mobile/auth/auth_screen.dart';
+import 'package:cs_mobile/auth/auth_root_widget.dart';
 import 'package:cs_mobile/firebase_options.dart';
 import 'package:cs_mobile/screens/main_screen/main_screen.dart';
+import 'package:cs_mobile/top_level_provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
+import 'routing/app_router.dart';
+import 'services/shared_prefrences.dart';
+
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  final sharedPreferences = await SharedPreferences.getInstance();
+  runApp(ProviderScope(
+    overrides: [
+      sharedPreferencesServiceProvider.overrideWithValue(
+        SharedPreferencesService(sharedPreferences),
+      ),
+    ],
+    child: MyApp(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({Key? key}) : super(key: key);
-
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final firebaseAuth = ref.watch(firebaseAuthProvider);
     return MaterialApp(
-      theme: ThemeData.dark(),
-      home: const MainScreen(),
+      debugShowCheckedModeBanner: false,
+      home: AuthWidget(
+        nonSignedInBuilder: (_) => Consumer(
+          builder: (context, ref, _) {
+            return GithubAuthScreen.withFirebaseAuth(firebaseAuth);
+          },
+        ),
+        signedInBuilder: (_) => MainScreen(),
+      ),
+      onGenerateRoute: (settings) =>
+          AppRouter.onGenerateRoute(settings, firebaseAuth),
     );
   }
 }

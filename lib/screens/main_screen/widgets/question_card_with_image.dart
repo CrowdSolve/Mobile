cf7ff89f0c2d3d73a5710f130bca9fffa9b6925e
Youssef Lasheen <youssef@lasheen.dev>
2022-04-16 23:35:51 +0200

    Add a hero to the question card with image

--- lib/screens/main_screen/widgets/question_card_with_image.dart
+++ lib/screens/main_screen/widgets/question_card_with_image.dart
@@ -20,124 +20,127 @@ class QuestionCardWithImage extends StatelessWidget {
       padding: const EdgeInsets.symmetric(vertical: 10),
       child: ConstrainedBox(
         constraints: BoxConstraints.tightFor(height: 400),
-        child: Card(
-          clipBehavior: Clip.antiAlias,
-          child: InkWell(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => QuestionDetails(
-                    id: question.id,
-                    question: question,
-                  ),
-                ),
-              );
-            },
-            child: Column(
-              children: [
-                Expanded(
-                  child: SizedBox.expand(
-                      child:
-                          Image.network(question.imageUrl!, fit: BoxFit.cover)),
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 15.0, vertical: 10),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Flexible(
-                          child: Text(question.title,
-                              style: Theme.of(context).textTheme.titleLarge,
-                              maxLines: 2,
-                              overflow: TextOverflow.ellipsis),
-                        ),
-                        Flexible(
-                          child: Text(body,
-                              style: Theme.of(context).textTheme.bodyLarge,
-                              overflow: TextOverflow.fade),
-                        ),
-                        SizedBox(
-                          height: 50,
-                          child: Column(
-                            children: [
-                              Expanded(
-                                child: Row(
-                                  children: [
-                                    Expanded(
-                                      flex: 3,
-                                      child: Row(
-                                        children: [
-                                          CircleAvatar(
-                                            backgroundImage: NetworkImage(
-                                                question.posterAvatarUrl),
-                                            radius: 10,
-                                          ),
-                                          SizedBox(
-                                            width: 10,
-                                          ),
-                                          Text(
-                                            question.posterName,
-                                            style: Theme.of(context)
-                                                .textTheme
-                                                .titleSmall,
-                                          ),
-                                          Text(
-                                              ' ● ${f.format(DateTime.parse(question.createdAt))}',
-                                              style: Theme.of(context)
-                                                  .textTheme
-                                                  .labelSmall),
-                                        ],
-                                      ),
-                                    ),
-                                    Expanded(
-                                      flex: 2,
-                                      child: Row(
-                                        children: [
-                                          Icon(
-                                            Icons.favorite,
-                                          ),
-                                          SizedBox(
-                                            width: 5,
-                                          ),
-                                          Text(
-                                            question.heart.toString(),
-                                            style: Theme.of(context)
-                                                .textTheme
-                                                .labelMedium,
-                                          ),
-                                          SizedBox(
-                                            width: 30,
-                                          ),
-                                          Icon(
-                                            Icons.comment,
-                                          ),
-                                          SizedBox(
-                                            width: 5,
-                                          ),
-                                          Text(
-                                            question.noOfComments.toString(),
-                                            style: Theme.of(context)
-                                                .textTheme
-                                                .labelMedium,
-                                          ),
-                                        ],
-                                      ),
-                                    )
-                                  ],
-                                ),
-                              )
-                            ],
-                          ),
-                        ),
-                      ],
+        child: Hero(
+          tag: 'question' + question.id.toString(),
+          child: Card(
+            clipBehavior: Clip.antiAlias,
+            child: InkWell(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => QuestionDetails(
+                      id: question.id,
+                      question: question,
                     ),
                   ),
-                )
-              ],
+                );
+              },
+              child: Column(
+                children: [
+                  Expanded(
+                    child: SizedBox.expand(
+                        child:
+                            Image.network(question.imageUrl!, fit: BoxFit.cover)),
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 15.0, vertical: 10),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Flexible(
+                            child: Text(question.title,
+                                style: Theme.of(context).textTheme.titleLarge,
+                                maxLines: 2,
+                                overflow: TextOverflow.ellipsis),
+                          ),
+                          Flexible(
+                            child: Text(body,
+                                style: Theme.of(context).textTheme.bodyLarge,
+                                overflow: TextOverflow.fade),
+                          ),
+                          SizedBox(
+                            height: 50,
+                            child: Column(
+                              children: [
+                                Expanded(
+                                  child: Row(
+                                    children: [
+                                      Expanded(
+                                        flex: 3,
+                                        child: Row(
+                                          children: [
+                                            CircleAvatar(
+                                              backgroundImage: NetworkImage(
+                                                  question.posterAvatarUrl),
+                                              radius: 10,
+                                            ),
+                                            SizedBox(
+                                              width: 10,
+                                            ),
+                                            Text(
+                                              question.posterName,
+                                              style: Theme.of(context)
+                                                  .textTheme
+                                                  .titleSmall,
+                                            ),
+                                            Text(
+                                                ' ● ${f.format(DateTime.parse(question.createdAt))}',
+                                                style: Theme.of(context)
+                                                    .textTheme
+                                                    .labelSmall),
+                                          ],
+                                        ),
+                                      ),
+                                      Expanded(
+                                        flex: 2,
+                                        child: Row(
+                                          children: [
+                                            Icon(
+                                              Icons.favorite,
+                                            ),
+                                            SizedBox(
+                                              width: 5,
+                                            ),
+                                            Text(
+                                              question.heart.toString(),
+                                              style: Theme.of(context)
+                                                  .textTheme
+                                                  .labelMedium,
+                                            ),
+                                            SizedBox(
+                                              width: 30,
+                                            ),
+                                            Icon(
+                                              Icons.comment,
+                                            ),
+                                            SizedBox(
+                                              width: 5,
+                                            ),
+                                            Text(
+                                              question.noOfComments.toString(),
+                                              style: Theme.of(context)
+                                                  .textTheme
+                                                  .labelMedium,
+                                            ),
+                                          ],
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ),

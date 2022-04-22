b0a3de2fdabf173a63d63d1c26ce0071ef94c91f
Youssef Lasheen <youssef@lasheen.dev>
2022-04-22 16:27:38 +0200

    Confirm with the user before exiting question edit

--- lib/screens/main_screen/widgets/add_question.dart
+++ lib/screens/main_screen/widgets/add_question.dart
@@ -36,6 +36,30 @@ class _AddQuestionState extends ConsumerState<AddQuestion> {
 
   bool _loading = false;
 
+  Future<bool> _onWillPop() async {
+    return await showDialog(
+            context: context,
+            builder: (context) => AlertDialog(
+                  title: Text('Unsaved changes'),
+                  content: Text(
+                      'Do you really want to discard your current changes?'),
+                  actions: <Widget>[
+                    FlatButton(
+                      child: Text('Cancel'),
+                      onPressed: () {
+                        Navigator.of(context).pop(false);
+                      },
+                    ),
+                    FlatButton(
+                      child: Text('Discard'),
+                      onPressed: () {
+                        Navigator.of(context).pop(true);
+                      },
+                    )
+                  ],
+                )) ??
+        false;
+  }
   void _insertText(String inserted, TextEditingController _controller) {
       final text = _controller.text;
       final selection = _controller.selection;
@@ -108,118 +132,125 @@ class _AddQuestionState extends ConsumerState<AddQuestion> {
       }
     }
     var inputDecoration = InputDecoration(border: InputBorder.none);
-    return Scaffold(
-      body:Column(
-        children: [
-          SizedBox(
-            height: 56,
-            child: Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16),
-                  child: IconButton(
-                    icon: const Icon(
-                      Icons.close_rounded,
-                      size: 24,
-                    ),
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                  ),
-                ),
-                Text(
-                  "Ask a question",
-                  style: Theme.of(context).textTheme.titleLarge,
-                ),
-                const Spacer(),
-                IconButton(
-                    onPressed: !_loading?() => _onImageButtonPressed():null,
-                    icon: !_loading?Icon(Icons.attach_file_rounded):CircularProgressIndicator()),
-                TextButton(
-                  onPressed:
-                      _validated && !_loading? () => _confirmSubmit(context) : null,
-                  child: Text("Save"),
-                )
-              ],
-            ),
-          ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Form(
-                key: _formKey,
-                autovalidateMode: AutovalidateMode.onUserInteraction,                
-                onChanged: (){
-                  bool _condition = !(_titleController.text.isEmpty || _bodyController.text.isEmpty);
-                  if(_validated !=_condition){
-                    setState(() {
-                      _validated = _condition;
-                    });
-                  }
-                },
-                child: Column(
-                  children: [
-                    TextFormField(
-                      controller: _titleController,
-                      focusNode: _titleFocusNode,
-                      maxLength: 20,
-                      maxLines: 1,
-                      decoration: inputDecoration.copyWith(hintText: "Title", ),
-                      style: TextStyle(fontSize: 20),
-                    ),
-                    Expanded(
-                      child: TextFormField(
-                        selectionControls: FlutterSelectionControls(verticalPadding: 14, horizontalPadding: 16,toolBarItems: <ToolBarItem>[
-                          ToolBarItem(
-                              item: Text(
-                                'Select All',
-                              ),
-                              itemControl: ToolBarItemControl.selectAll),
-                          ToolBarItem(
-                              item: Text(
-                                'Copy',
-                              ),
-                              itemControl: ToolBarItemControl.copy),
-                          ToolBarItem(
-                              item: Text(
-                                'Paste',
-                              ),
-                              itemControl: ToolBarItemControl.paste),
-                          ToolBarItem(
-                              item: Text(
-                                'Bold',
-                              ),
-                              onItemPressed: (content, start, end) =>
-                                    wrapText(content, start, end, '**'),
-                              ),
-                              ToolBarItem(
-                                  item: Text(
-                                    'Italic',
-                                  ),
-                              onItemPressed: (content, start, end) =>
-                                    wrapText(content, start, end, '*'),
-                              ),
-                          ToolBarItem(
-                              item: Text(
-                                'Strikethrough',
-                              ),
-                              onItemPressed: (content, start, end) =>
-                                    wrapText(content, start, end, '~~'),
-                              ),
-                        ]),
-                        controller: _bodyController,
-                        focusNode: _bodyFocusNode,
-                        maxLines: 99,
-                        decoration: inputDecoration.copyWith(hintText: "Body"),
+    return WillPopScope(
+      onWillPop: _onWillPop,
+      child: Scaffold(
+        body:Column(
+          children: [
+            SizedBox(
+              height: 56,
+              child: Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: IconButton(
+                      icon: const Icon(
+                        Icons.close_rounded,
+                        size: 24,
                       ),
+                      onPressed: () async =>
+                            await _onWillPop() ? Navigator.pop(context) : null
                     ),
-                  ],
-                ),
+                  ),
+                  Text(
+                    "Ask a question",
+                    style: Theme.of(context).textTheme.titleLarge,
+                  ),
+                  const Spacer(),
+                  IconButton(
+                      onPressed: !_loading?() => _onImageButtonPressed():null,
+                      icon: !_loading?Icon(Icons.attach_file_rounded):CircularProgressIndicator()),
+                  TextButton(
+                    onPressed:
+                        _validated && !_loading? () => _confirmSubmit(context) : null,
+                    child: Text("Save"),
+                  )
+                ],
               ),
             ),
-          )
-        ],
-      ), 
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Form(
+                  key: _formKey,
+                  autovalidateMode: AutovalidateMode.onUserInteraction,                
+                  onChanged: (){
+                    bool _condition = !(_titleController.text.isEmpty || _bodyController.text.isEmpty);
+                    if(_validated !=_condition){
+                      setState(() {
+                        _validated = _condition;
+                      });
+                    }
+                  },
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        controller: _titleController,
+                        focusNode: _titleFocusNode,
+                        maxLength: 20,
+                        maxLines: 1,
+                        decoration: inputDecoration.copyWith(hintText: "Title", ),
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      Expanded(
+                        child: TextFormField(
+                          selectionControls: FlutterSelectionControls(verticalPadding: 14, horizontalPadding: 16,toolBarItems: <ToolBarItem>[
+                            ToolBarItem(
+                                item: Text(
+                                  'Cut',
+                                ),
+                                itemControl: ToolBarItemControl.cut),
+                            ToolBarItem(
+                                item: Text(
+                                  'Copy',
+                                ),
+                                itemControl: ToolBarItemControl.copy),
+                            ToolBarItem(
+                                item: Text(
+                                  'Paste',
+                                ),
+                                itemControl: ToolBarItemControl.paste),
+                            ToolBarItem(
+                                item: Text(
+                                  'Select All',
+                                ),
+                                itemControl: ToolBarItemControl.selectAll),
+                            ToolBarItem(
+                                item: Text(
+                                  'Bold',
+                                ),
+                                onItemPressed: (content, start, end) =>
+                                      wrapText(content, start, end, '**'),
+                                ),
+                                ToolBarItem(
+                                    item: Text(
+                                      'Italic',
+                                    ),
+                                onItemPressed: (content, start, end) =>
+                                      wrapText(content, start, end, '*'),
+                                ),
+                            ToolBarItem(
+                                item: Text(
+                                  'Strikethrough',
+                                ),
+                                onItemPressed: (content, start, end) =>
+                                      wrapText(content, start, end, '~~'),
+                                ),
+                          ]),
+                          controller: _bodyController,
+                          focusNode: _bodyFocusNode,
+                          maxLines: 99,
+                          decoration: inputDecoration.copyWith(hintText: "Body"),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
+        ), 
+      ),
     );
   }
 

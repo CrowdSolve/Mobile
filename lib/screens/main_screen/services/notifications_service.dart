bc8f0f05aef6a3dafc047e668d33e989794a264f
Youssef Lasheen <youssef@lasheen.dev>
2022-07-18 02:32:36 +0200

    Create the service file for notifiacition to be able to fetch them

---
+++ lib/screens/main_screen/services/notifications_service.dart
@@ -0,0 +1,30 @@
+
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+import 'package:cs_mobile/screens/main_screen/models/notification.dart';
+
+Future<List<NotificationModel>> fetchNotifications(String authKey, int pageKey) async {
+  final response = await http.get(
+    Uri.parse('https://api.github.com/repos/CrowdSolve/data/notifications?all=true&page=$pageKey'),
+    headers: <String, String>{
+      'Accept': 'application/vnd.github+json',
+      'Authorization': 'Bearer ' + authKey,
+    },
+  );
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    var tagObjsJson = jsonDecode(response.body) as List;
+    List<NotificationModel> tagObjs =
+        tagObjsJson.map((tagJson) => NotificationModel.fromJson(tagJson)).toList();
+    return tagObjs;
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    print(response.body);
+    throw Exception('Failed to load album');
+  }
+}

e718f47febb52b4a0330d8945157833e372bd87c
Youssef Lasheen <youssef@lasheen.dev>
2022-07-21 02:50:29 +0200

    Make the profile dialog scrollable

--- lib/screens/main_screen/widgets/search_button/widgets/profile_dialog.dart
+++ lib/screens/main_screen/widgets/search_button/widgets/profile_dialog.dart
@@ -43,91 +43,96 @@ class ProfileDialog extends ConsumerWidget  {
     final firebaseAuth = ref.watch(firebaseAuthProvider);
     final githubOAuthKeyModel = ref.watch(githubOAuthKeyModelProvider);
     return SafeArea(
-      child: Dialog(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.vertical(top: Radius.circular(10))),
-        elevation: 10,
-        insetPadding: EdgeInsets.only(top: 75, right: 15, left: 15),
-        child: Padding(
-          padding: const EdgeInsets.only(top: 10, right: 15, left: 15),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Align(
-                alignment: Alignment.topLeft,
-                child: IconButton(
-                  alignment: Alignment.centerLeft,
-                  padding: EdgeInsets.zero,
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                    icon: Icon(Icons.close_rounded)),
-              ),
-              SizedBox(height: 20,),
-              SizedBox(
-                height: 35,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Hero(
-                      tag: 'avatar',
-                      child: CircleAvatar(
-                        backgroundColor: Colors.transparent,
-                        backgroundImage: NetworkImage(user.avatarUrl),
+      child: SingleChildScrollView(
+        child: Center(
+          child: SizedBox(
+            height: 500,
+            width: double.infinity,
+            child: Dialog(
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.vertical(top: Radius.circular(10))),
+              elevation: 10,
+              insetPadding: EdgeInsets.only(top: 75, right: 15, left: 15),
+              child: Padding(
+                padding: const EdgeInsets.only(top: 10, right: 15, left: 15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: IconButton(
+                        alignment: Alignment.centerLeft,
+                        padding: EdgeInsets.zero,
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          icon: Icon(Icons.close_rounded)),
+                    ),
+                    SizedBox(height: 20,),
+                    SizedBox(
+                      height: 35,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Hero(
+                            tag: 'avatar',
+                            child: CircleAvatar(
+                              backgroundColor: Colors.transparent,
+                              backgroundImage: NetworkImage(user.avatarUrl),
+                            ),
+                          ),
+                          SizedBox(
+                            width: 22,
+                          ),
+                          user.name==""?
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Youssef Lasheen',
+                                style: TextStyle(
+                                    fontSize: 15, fontWeight: FontWeight.normal,),
+                              ),
+                              Text(
+                                user.login,
+                                style: TextStyle(
+                                    fontSize: 11,
+                                    fontWeight: FontWeight.w600,
+                                    color: Colors.grey),
+                              ),
+                            ],
+                          ):Text(
+                                user.login,
+                                style: TextStyle(
+                                    fontSize: 15, fontWeight: FontWeight.normal),
+                              ),
+                          
+                          
+                        ],
                       ),
                     ),
-                    SizedBox(
-                      width: 22,
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10),
+                      child: Divider(),
                     ),
-                    user.name==""?
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Youssef Lasheen',
-                          style: TextStyle(
-                              fontSize: 15, fontWeight: FontWeight.normal,),
-                        ),
-                        Text(
-                          user.login,
-                          style: TextStyle(
-                              fontSize: 11,
-                              fontWeight: FontWeight.w600,
-                              color: Colors.grey),
-                        ),
-                      ],
-                    ):Text(
-                          user.login,
-                          style: TextStyle(
-                              fontSize: 15, fontWeight: FontWeight.normal),
-                        ),
-                    
-                    
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 10),
-                child: Divider(),
-              ),
-              ListTile(
-                leading: Icon(Icons.my_library_books),
-                title: Text('My questions'),
-                onTap: ()=> Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => MyQuestions(user.login)),
-                ),
-              ),
-              ListTile(
-                leading: Icon(Icons.notifications),
-                title: Text('My Notifications'),
-                onTap: ()=> Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => MyNotifications(githubOAuthKeyModel)),
-                ),
-              ),
-              Spacer(),
+                    ListTile(
+                      leading: Icon(Icons.my_library_books),
+                      title: Text('My questions'),
+                      onTap: ()=> Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => MyQuestions(user.login)),
+                      ),
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.notifications),
+                      title: Text('My Notifications'),
+                      onTap: ()=> Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => MyNotifications(githubOAuthKeyModel)),
+                      ),
+                    ),
+                    Spacer(),
                     Center(
                       child: TextButton(
                         style: TextButton.styleFrom(),
@@ -135,8 +140,13 @@ class ProfileDialog extends ConsumerWidget  {
                           "Logout",
                           style: TextStyle(color: Colors.red),
                         ),
-              )
-            ],
+                        onPressed: () => _confirmSignOut(context, firebaseAuth),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ),
         ),
       ),

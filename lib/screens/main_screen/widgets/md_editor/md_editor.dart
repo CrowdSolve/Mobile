aa187ffba5cd4ed13098b33904826e6d203ff396
Youssef Lasheen <youssef@lasheen.dev>
2022-09-10 01:01:39 +0200

    Add a category chooser in the appbar

--- lib/screens/main_screen/widgets/md_editor/md_editor.dart
+++ lib/screens/main_screen/widgets/md_editor/md_editor.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:alert_dialogs/alert_dialogs.dart';
+import 'package:cs_mobile/screens/main_screen/screens/widgets/categories_dialog.dart';
 import 'package:cs_mobile/screens/main_screen/services/questions_service.dart';
 import 'package:cs_mobile/top_level_provider.dart';
 import 'package:flutter/material.dart';
@@ -56,6 +57,8 @@ class _AddQuestionState extends ConsumerState<MDEditor> {
     }
   }
 
+  String query = "";
+
   @override
   Widget build(BuildContext context) {
     final githubOAuthKeyModel = ref.watch(githubOAuthKeyModelProvider);
@@ -69,108 +72,136 @@ class _AddQuestionState extends ConsumerState<MDEditor> {
         sizeFactor: animation,
         child: Container(color: Theme.of(context).colorScheme.background),
       ),
-      child: SafeArea(
-        child: WillPopScope(
-          onWillPop: (() => onWillPop(context)),
-          child: Scaffold(
-            backgroundColor: Theme.of(context).colorScheme.background,
-            appBar: AppBar(
-              title: Text(widget.isTitleIncluded?'Ask a question':'Add a comment'),
-              leading: IconButton(
-                  icon: const Icon(
-                    Icons.close_rounded,
-                    size: 24,
-                  ),
-                  onPressed: () async =>
-                      await onWillPop(context) ? Navigator.pop(context) : null),
-              actions: [
-                TextButton(
-                  onPressed:_validated && !_loading? () {
-                      widget.isTitleIncluded
-                          ? confirmSubmitQuestion(context, githubOAuthKeyModel,_titleController.text, _bodyController.text)
-                          : confirmSubmitComment(context, githubOAuthKeyModel,_bodyController.text, widget.questionId!);
-                      
-                  }:null,
-                  child: Text("Save"),
-                )
-              ],
-            ),
-            body: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Form(
-                key: _formKey,
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-                onChanged: () {
-                  //Check if form is validated and update state
-                  bool _condition = (_bodyController.text.isNotEmpty && (_titleController.text.isNotEmpty||!widget.isTitleIncluded));
-                  if (_validated != _condition) {
-                    setState(() {
-                      _validated = _condition;
-                    });
-                  }
-                },
-                child: Column(
+      child: WillPopScope(
+        onWillPop: (() => onWillPop(context)),
+        child: Scaffold(
+          backgroundColor: Theme.of(context).colorScheme.background,
+          appBar: AppBar(
+            title: Text(widget.isTitleIncluded?'Ask a question':'Add a comment'),
+            leading: IconButton(
+                icon: const Icon(
+                  Icons.close_rounded,
+                  size: 24,
+                ),
+                onPressed: () async =>
+                    await onWillPop(context) ? Navigator.pop(context) : null),
+            titleSpacing: 6,
+            actions: [
+              if(widget.isTitleIncluded)
+              TextButton(
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
                   children: [
-                    widget.isTitleIncluded?
-                    TextFormField(
-                      controller: _titleController,
-                      focusNode: _titleFocusNode,
-                      maxLength: 20,
-                      maxLines: 1,
-                      decoration: inputDecoration.copyWith(
-                        hintText: "Title",
-                      ),
-                      style: TextStyle(fontSize: 20),
-                    ):SizedBox.shrink(),
-                    Expanded(
-                      child: TextFormField(
-                        selectionControls: FlutterSelectionControls(
-                            verticalPadding: 14,
-                            horizontalPadding: 12,
-                            toolBarItems: <ToolBarItem>[
-                              ToolBarItem(
-                                  item: Icon(Icons.cut_rounded),
-                                  itemControl: ToolBarItemControl.cut),
-                              ToolBarItem(
-                                  item: Icon(Icons.copy_rounded),
-                                  itemControl: ToolBarItemControl.copy),
-                              ToolBarItem(
-                                  item: Icon(Icons.paste_rounded),
-                                  itemControl: ToolBarItemControl.paste),
-                              ToolBarItem(
-                                  item: Icon(Icons.select_all_rounded),
-                                  itemControl: ToolBarItemControl.selectAll),
-                              ToolBarItem(
-                                item: Icon(Icons.format_bold_rounded),
-                                onItemPressed: (content, start, end) => wrapText(
-                                    _bodyController, content, start, end, '**'),
-                              ),
-                              ToolBarItem(
-                                item: Icon(Icons.format_italic_rounded),
-                                onItemPressed: (content, start, end) => wrapText(
-                                    _bodyController, content, start, end, '*'),
-                              ),
-                              ToolBarItem(
-                                item: Icon(Icons.format_strikethrough_rounded),
-                                onItemPressed: (content, start, end) => wrapText(
-                                    _bodyController, content, start, end, '~~'),
-                              ),
-                              ToolBarItem(
-                                item: !_loading
-                                    ? Icon(Icons.add_photo_alternate_rounded)
-                                    : CircularProgressIndicator(),
-                                onItemPressed: (content, start, end) =>
-                                    !_loading ? _onImageButtonPressed() : null,
-                              ),
-                            ]),
-                        controller: _bodyController,
-                        focusNode: _bodyFocusNode,
-                        maxLines: 99,
-                        decoration: inputDecoration.copyWith(hintText: "Body"),
-                      ),
+                    ConstrainedBox(
+                      constraints: BoxConstraints(maxWidth: 100),
+                      child: Text(query.isEmpty
+                          ? "No Label"
+                          : query.substring(2), style: TextStyle(fontSize: 12),overflow: TextOverflow.ellipsis),
                     ),
+                    Icon(Icons.arrow_drop_down),
                   ],
                 ),
+                style: TextButton.styleFrom(padding: EdgeInsets.zero,),
+                onPressed: () async {
+                  query = await Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              fullscreenDialog: true,
+                              builder: (context) => CategoriesDialog())) ??
+                      "";
+                  setState(() {});
+                },
+              ),
+              IconButton(
+                style: TextButton.styleFrom(visualDensity: VisualDensity.compact,),
+                onPressed:_validated && !_loading? () {
+                    widget.isTitleIncluded
+                        ? confirmSubmitQuestion(context, githubOAuthKeyModel,_titleController.text, _bodyController.text)
+                        : confirmSubmitComment(context, githubOAuthKeyModel,_bodyController.text, widget.questionId!);
+                    
+                }:null,
+                icon: Icon(
+                  Icons.send_rounded,
+                  size: 22,
+                ),
+              )
+            ],
+          ),
+          body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Form(
+              key: _formKey,
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              onChanged: () {
+                //Check if form is validated and update state
+                bool _condition = (_bodyController.text.isNotEmpty && (_titleController.text.isNotEmpty||!widget.isTitleIncluded));
+                if (_validated != _condition) {
+                  setState(() {
+                    _validated = _condition;
+                  });
+                }
+              },
+              child: Column(
+                children: [
+                  widget.isTitleIncluded?
+                  TextFormField(
+                    controller: _titleController,
+                    focusNode: _titleFocusNode,
+                    maxLength: 20,
+                    maxLines: 1,
+                    decoration: inputDecoration.copyWith(
+                      hintText: "Title",
+                    ),
+                    style: TextStyle(fontSize: 20),
+                  ):SizedBox.shrink(),
+                  Expanded(
+                    child: TextFormField(
+                      selectionControls: FlutterSelectionControls(
+                          verticalPadding: 14,
+                          horizontalPadding: 12,
+                          toolBarItems: <ToolBarItem>[
+                            ToolBarItem(
+                                item: Icon(Icons.cut_rounded),
+                                itemControl: ToolBarItemControl.cut),
+                            ToolBarItem(
+                                item: Icon(Icons.copy_rounded),
+                                itemControl: ToolBarItemControl.copy),
+                            ToolBarItem(
+                                item: Icon(Icons.paste_rounded),
+                                itemControl: ToolBarItemControl.paste),
+                            ToolBarItem(
+                                item: Icon(Icons.select_all_rounded),
+                                itemControl: ToolBarItemControl.selectAll),
+                            ToolBarItem(
+                              item: Icon(Icons.format_bold_rounded),
+                              onItemPressed: (content, start, end) => wrapText(
+                                  _bodyController, content, start, end, '**'),
+                            ),
+                            ToolBarItem(
+                              item: Icon(Icons.format_italic_rounded),
+                              onItemPressed: (content, start, end) => wrapText(
+                                  _bodyController, content, start, end, '*'),
+                            ),
+                            ToolBarItem(
+                              item: Icon(Icons.format_strikethrough_rounded),
+                              onItemPressed: (content, start, end) => wrapText(
+                                  _bodyController, content, start, end, '~~'),
+                            ),
+                            ToolBarItem(
+                              item: !_loading
+                                  ? Icon(Icons.add_photo_alternate_rounded)
+                                  : CircularProgressIndicator(),
+                              onItemPressed: (content, start, end) =>
+                                  !_loading ? _onImageButtonPressed() : null,
+                            ),
+                          ]),
+                      controller: _bodyController,
+                      focusNode: _bodyFocusNode,
+                      maxLines: 99,
+                      decoration: inputDecoration.copyWith(hintText: "Body"),
+                    ),
+                  ),
+                ],
               ),
             ),
           ),

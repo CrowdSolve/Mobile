2e87fcf19eeb495062a3a2b467b4e77178b7aee2
Youssef Lasheen <youssef@lasheen.dev>
2022-09-25 17:20:44 +0200

    Fix using the right id for creating the comment

--- lib/screens/questions_screen/shared_components/question_details/question_details.dart
+++ lib/screens/questions_screen/shared_components/question_details/question_details.dart
@@ -67,7 +67,7 @@ class _QuestionDetailsState extends State<QuestionDetails> {
       ),
       child: Scaffold(
         backgroundColor: Theme.of(context).colorScheme.surface,
-        floatingActionButton: AnimatedFAB(openWidget: MDEditor.comment(questionId: widget.id,), label: 'Answer the question', icon: Icons.add_comment ,),
+        floatingActionButton: AnimatedFAB(openWidget: MDEditor.comment(questionId: widget.id !='q'?widget.id:widget.question!.id,), label: 'Answer the question', icon: Icons.add_comment ,),
         body: ListView(
           children: [
             widget.question != null?ExpandedQuestionCard(question: widget.question!):

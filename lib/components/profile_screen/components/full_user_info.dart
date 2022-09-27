34c7ab170a083b84710c315e603f3f30647c0333
Youssef Lasheen <youssef@lasheen.dev>
2022-09-27 02:40:36 +0200

    Use the new graphql pachage to generate some info about the user

--- lib/components/profile_screen/components/full_user_info.dart
+++ lib/components/profile_screen/components/full_user_info.dart
@@ -1,26 +1,49 @@
 import 'package:cs_mobile/models/user.dart';
 import 'package:cs_mobile/services/user_service.dart';
 import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
-class FullUserInfo extends StatefulWidget {
-  final String userId;
+class FullUserInfo extends StatelessWidget {
+  final String userLogin;
 
-  const FullUserInfo({Key? key, required this.userId}) : super(key: key);
-
-  @override
-  State<FullUserInfo> createState() => _FullUserInfoState();
-}
-
-class _FullUserInfoState extends State<FullUserInfo> {
-  late Future<FullUserModel> fullUser;
-  @override
-  void initState() {
-    fullUser = fetchUserWithId(widget.userId);
-    super.initState();
-  }
+  FullUserInfo({Key? key, required this.userLogin}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    String graph = '''
+          query (\$authorQuery: String!, \$involvedQuery: String!, \$mostLikesQuery: String!) {
+              authored: search(type: ISSUE, query: \$authorQuery) {
+                issueCount
+              }
+              involved: search(type: ISSUE, query: \$involvedQuery) {
+                issueCount
+              }
+              mostLikes: search(type: ISSUE, query: \$mostLikesQuery, first: 1) {
+                edges {
+                  node {
+                    ... on Issue {
+                      reactions {
+                        totalCount
+                      }
+                    }
+                  }
+                }
+              }
+              user(login: "$userLogin") {
+                bio
+                location
+                company
+                email
+                name
+                avatarUrl
+                id
+                websiteUrl
+                company,
+                login
+                
+              }
+            }
+    ''';
     return Container(
       decoration: BoxDecoration(
         color: Theme.of(context).colorScheme.secondaryContainer,
@@ -28,63 +51,102 @@ class _FullUserInfoState extends State<FullUserInfo> {
       ),
       child: SizedBox(
         height: 130,
-        child: FutureBuilder<FullUserModel>(
-          future: fullUser,
-          builder: (context, snapshot) {
-            FullUserModel? user= snapshot.data; 
+        child: Query(
+          options: QueryOptions(
+              fetchPolicy: FetchPolicy.cacheAndNetwork,
+              document: gql(graph),
+              variables: {
+                "authorQuery": "author:$userLogin repo:crowdsolve/data",
+                "involvedQuery": "involves:$userLogin repo:crowdsolve/data",
+                "mostLikesQuery":
+                    "author:$userLogin repo:crowdsolve/data, sort:reactions-desc"
+              }),
+          builder: (QueryResult result,
+              {VoidCallback? refetch, FetchMore? fetchMore}) {
+            if (result!.hasException) {
+              return Text(result.exception.toString());
+            }
+            int? authoredCount, involvedCount,mostLikeOnASingleQuestion;
+            FullUserModel? user;
+            if (result.isNotLoading) {
+              var data = result.data!;
+
+               authoredCount = data['authored']?['issueCount'];
+               involvedCount = data['involved']?['issueCount'];
+               mostLikeOnASingleQuestion = data['mostLikes']?['edges']?[0]
+                  ?['node']?['reactions']?['totalCount'];
+              //bool isMember = data['organization']?['viewerIsAMember'];
+              //bool isAdmin = data['organization']?['viewerCanAdminister'];
+              var userData = data['user'];
+               user = FullUserModel(
+                name: userData?['name'] ?? '',
+                email: userData?['email'] ?? '',
+                bio: userData?['bio'] ?? '',
+                avatarUrl: userData?['avatarUrl'],
+                id: userData?['id'],
+                location: userData?['location'] ?? '',
+                inistitution: userData?['company'] ?? '',
+                login: userData?['login'],
+                website: userData?['websiteUrl'] ?? '',
+              );
+            }
+            
+
             return Column(
               children: [
-              Container(
-                height: 40,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  color: Theme.of(context).colorScheme.background,
-                  borderRadius:
-                      BorderRadius.vertical(bottom: Radius.circular(40)),
-                  boxShadow: [
-                    BoxShadow(
-                      offset: Offset(0, 5),
-                      color: Theme.of(context)
-                          .colorScheme
-                          .shadow
-                          .withOpacity(0.5),
-                    ),
-                  ],
-                ),
-                child: user != null? Center(
-                  child: Text(
-                    user.name,
-                    style: Theme.of(context).textTheme.subtitle1!.copyWith(
-                          color: Theme.of(context).colorScheme.onBackground,
+                Container(
+                    height: 40,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).colorScheme.background,
+                      borderRadius:
+                          BorderRadius.vertical(bottom: Radius.circular(40)),
+                      boxShadow: [
+                        BoxShadow(
+                          offset: Offset(0, 5),
+                          color: Theme.of(context)
+                              .colorScheme
+                              .shadow
+                              .withOpacity(0.5),
                         ),
-                  ),
-                ):SizedBox.shrink(),
-              ),
-              Spacer(),
-              if (user != null)
-                Row(
-                  children: [
-                    if (user.email.isNotEmpty)
-                      InfoTile(info: user.email, icon: Icons.email_rounded),
-                    SizedBox(
-                      width: 20,
+                      ],
                     ),
-                    if (user.location.isNotEmpty)
+                    child: Center(
+                      child: user !=null? Text(
+                        user.name,
+                        style: Theme.of(context).textTheme.subtitle1!.copyWith(
+                              color: Theme.of(context).colorScheme.onBackground,
+                            ),
+                      ):CircularProgressIndicator(),
+                    )),
+                Spacer(),
+                if(result.isNotLoading)
+                IntrinsicHeight(
+                  child: Row(
+                    children: [
+                      Spacer(),
                       InfoTile(
-                          info: user.location,
-                          icon: Icons.location_on_rounded),
-                    SizedBox(
-                      width: 20,
-                    ),
-                    if (user.bio.isNotEmpty)
-                      InfoTile(info: user.bio, icon: Icons.article_rounded),
-                  ],
-                )
-              else
-                const Center(child: CircularProgressIndicator())
-
-              ,Spacer()
-            ]);
+                          info: authoredCount.toString(), title: 'AUTHORED'),
+                      VerticalDivider(
+                        color:
+                            Theme.of(context).colorScheme.onSecondaryContainer,
+                      ),
+                      InfoTile(
+                          info: involvedCount.toString(), title: 'ANSWERED'),
+                      VerticalDivider(
+                        color:
+                            Theme.of(context).colorScheme.onSecondaryContainer,
+                      ),
+                      InfoTile(
+                          info: mostLikeOnASingleQuestion.toString(),
+                          title: 'LIKES'),
+                      Spacer(),
+                    ],
+                  ),
+                ),
+                Spacer()
+              ],
+            );
           },
         ),
       ),
@@ -93,19 +155,35 @@ class _FullUserInfoState extends State<FullUserInfo> {
 }
 
 class InfoTile extends StatelessWidget {
+  final String title;
   final String info;
-  final IconData icon;
 
-  const InfoTile({Key? key, required this.info, required this.icon})
-      : super(key: key);
+  const InfoTile({
+    Key? key,
+    required this.info,
+    required this.title,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
+      flex: 2,
       child: Column(
         children: [
-          Icon(icon, color: Theme.of(context).colorScheme.onSecondaryContainer,),
-          Text(info,style: TextStyle(color: Theme.of(context).colorScheme.onSecondaryContainer),),
+          Text(
+            info,
+            style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onSecondaryContainer,
+                  fontWeight: FontWeight.bold,
+                ),
+          ),
+          Text(
+            title,
+            maxLines: 1,
+            style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                  color: Theme.of(context).colorScheme.onSecondaryContainer,
+                ),
+          ),
         ],
       ),
     );

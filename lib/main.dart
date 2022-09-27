9f988440ce58786998b0c62e7ffd14c8268899b3
Youssef Lasheen <youssef@lasheen.dev>
2022-09-28 00:30:29 +0200

    Add a check for the latest version available and force the user to update in release mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:cs_mobile/auth/auth_root_widget.dart';
 import 'package:cs_mobile/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:in_app_update/in_app_update.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'services/shared_prefrences.dart';
@@ -15,6 +18,13 @@ Future<void> main() async {
   );
   final sharedPreferences = await SharedPreferences.getInstance();
   await initHiveForFlutter();
+  if (kReleaseMode)
+    await InAppUpdate.checkForUpdate().then((info) {
+      if (info.immediateUpdateAllowed)
+        InAppUpdate.performImmediateUpdate().then((result) {
+          if (result != AppUpdateResult.success) SystemNavigator.pop();
+        });
+    });
   runApp(ProviderScope(
     overrides: [
       sharedPreferencesServiceProvider.overrideWithValue(

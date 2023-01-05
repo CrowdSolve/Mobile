31324778e262f33a1e53dc3c3c91b6577ba41f06
Youssef Lasheen <youssef@lasheen.dev>
2023-01-05 16:45:48 +0200

    Use the new built in signInWithProvider instead of github sign in

--- lib/auth/auth_model.dart
+++ lib/auth/auth_model.dart
@@ -1,35 +1,26 @@
 import 'package:cs_mobile/top_level_provider.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-//import 'package:github_sign_in/github_sign_in.dart';
-import 'package:cs_mobile/auth/auth_service.dart';
-
-
 
 class AuthModel {
-  Future<void> authenticate(context, ref, {String? initialLogin, required bool allowSignUp}) async {
-    final GitHubSignIn gitHubSignIn = GitHubSignIn(
-      scope: 'public_repo, user, notifications',
-      clientId: '4b4d462397a576ac86fd',
-      clientSecret: '7bbde3c03020db1b1f31f6080e6823cc415086cd',
-      redirectUrl: 'https://crowd-solve.firebaseapp.com/__/auth/handler',
-      initialLogin: initialLogin,
-      allowSignUp: allowSignUp);
-    try {
-      //Show the sign in webview and get the token from it
-      final GitHubSignInResult signInResult = await gitHubSignIn.signIn(context);
+  Future<void> authenticate(context, ref,
+      {String? initialLogin, required bool allowSignUp}) async {
+    Future<UserCredential> signInWithGitHub() async {
+      GithubAuthProvider githubProvider = GithubAuthProvider();
+      githubProvider.addScope('public_repo');
+      githubProvider.addScope('user');
+      githubProvider.addScope('notifications');
+      githubProvider.setCustomParameters({
+        'allow_signup': '$allowSignUp',
+        'login': '$initialLogin',
+      });
 
-      //Check if sign in was successfull
-      if (signInResult.status.name != "ok") throw Exception(signInResult.errorMessage);
-
-      ref.watch(githubOAuthKeyModelProvider.notifier).setGithubOAuthKey(signInResult.token);
-
-      
-      //Login to google with the github credentials
-      await FirebaseAuth.instance
-          .signInWithCredential(GithubAuthProvider.credential(signInResult.token!));
-
-    } catch (e) {
-      rethrow;
+      return await FirebaseAuth.instance.signInWithProvider(githubProvider);
     }
+
+    UserCredential signInResult = await signInWithGitHub();
+
+    ref
+        .watch(githubOAuthKeyModelProvider.notifier)
+        .setGithubOAuthKey(signInResult.credential!.accessToken!);
   }
 }
